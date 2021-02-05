0bfba7458a2c2f2309473fb46866aef9a8fa7e5b
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-02-05 11:26:05 -0300

    Update TesteDesempenho.java

--- src/semana4/dia4/Collections/TesteDesempenho.java
+++ src/semana4/dia4/Collections/TesteDesempenho.java
@@ -11,24 +11,54 @@ public class TesteDesempenho {
 	 */
 	private static final int contElemento = 20000;
 	
-	public static void testaDesempenho(Collection<Integer> collection) {
-		long tInicio = System.currentTimeMillis();
-		
+	public static void insercao(Collection<Integer> collection) {
 		//fazendo inserções 
 		for (int i = 0; i < TesteDesempenho.contElemento; i++) {
 		      collection.add(i);
 		}
-		//realizando busca
+
+	}
+	public static void busca(Collection<Integer> collection) {
+		//fazendo inserções 
 		for (int i = 0; i < TesteDesempenho.contElemento; i++) {
-		      collection.contains(i);
+			collection.contains(i);
 		}
-		//realizando a remoção 
-		for (int i = TesteDesempenho.contElemento; i > -1; i--) {
-		      collection.remove(i);
+		
+	}
+	public static void remocao(Collection<Integer> collection) {
+		//fazendo inserções 
+		for (int i = 0; i < TesteDesempenho.contElemento; i++) {
+			collection.remove(i);
+		}
+		
+	}
+	
+	public static void testaDesempenho(Collection<Integer> collection, int opcao) {
+		/*
+		 * opção 1: testa inserção
+		 * opção 2: testa somente a busca,
+		 * opção 3: testa somente a remoção, 
+		 * default: testa inserção, busca e remoção
+		 * OBS: para testar as opçoes dois e tres é importante enviar a collection preenchida já
+		 */
+		long tInicio = System.currentTimeMillis();
+		switch (opcao) {
+			case 1: 
+				TesteDesempenho.insercao(collection);
+				break;
+			case 2:
+				TesteDesempenho.busca(collection);
+				break;
+			case 3: 
+				TesteDesempenho.remocao(collection);
+				break;
+			default:
+				TesteDesempenho.insercao(collection);
+				TesteDesempenho.busca(collection);
+				TesteDesempenho.remocao(collection);
 		}
 		long tFim = System.currentTimeMillis();
 	    System.out.println("Tempo total: " + (tFim - tInicio)+"ms\n");
-
 	}
 	
 	public static void main(String[] args) {
@@ -36,8 +66,8 @@ public class TesteDesempenho {
 		List<Integer> linkedList= new LinkedList<>();
 		
 		System.out.print("ArrayList: ");
-		TesteDesempenho.testaDesempenho(arrayList);
+		TesteDesempenho.testaDesempenho(arrayList, -1);
 		System.out.print("LinkedList: ");
-		TesteDesempenho.testaDesempenho(linkedList);
+		TesteDesempenho.testaDesempenho(linkedList, -1);
 	}
 }
