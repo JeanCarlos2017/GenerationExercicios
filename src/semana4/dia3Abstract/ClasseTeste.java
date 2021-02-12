14adf8c20d3c374b7c8bd2045b0a96978c67adfe
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-02-12 15:44:26 -0300

    Update ClasseTeste.java

--- src/semana4/dia3Abstract/ClasseTeste.java
+++ src/semana4/dia3Abstract/ClasseTeste.java
@@ -1,8 +1,21 @@
 package semana4.dia3Abstract;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class ClasseTeste {
+	
+	public static ArrayList<Animal> remove(ArrayList<Animal> vet, String nomeAnimal) {
+		//elimina o primeiro animal com o nome passado no parametro
+		ArrayList<Animal> temp = (ArrayList<Animal>) vet.stream()
+									.filter( animal -> !animal.getNome().equals(nomeAnimal))
+									.collect(Collectors.toList());
+		
+		return temp;
+		
+	}
+	
+
 	public static void main(String[] args) {
 		ArrayList<Animal> vet_animal = new ArrayList<>();
 		
@@ -11,21 +24,30 @@ public class ClasseTeste {
 		vet_animal.add(new Cachorro("lupi", 7));
 		vet_animal.add(new Cachorro("thor", 16));
 		vet_animal.add(new Cavalo("Carpeado", 20));
-		vet_animal.add(new Cavalo("Carpead2", 22));
-		vet_animal.add(new Cavalo("Carpead3", 26));
+		vet_animal.add(new Cavalo("Carpeado2", 22));
+		vet_animal.add(new Cavalo("Carpeado3", 26));
 		vet_animal.add(new Cavalo("Plotka", 15));
 		
 		//vet_animal.forEach( animal -> System.out.println(animal) );
-		System.out.println("Quantidade de animais: "+Animal.getQntAnimal());
+		//System.out.println("Quantidade de animais: "+Animal.getQntAnimal());
 		
 		//bixo preguiça 
 		vet_animal.add(new Preguica("Preguica", 40));
 		vet_animal.add(new Preguica("Preguica2", 36));
 		vet_animal.add(new Preguica("Preguica3", 2));
-		
 		vet_animal.forEach( animal -> System.out.println(animal) );
-		System.out.println("Quantidade de animais: "+Animal.getQntAnimal());
+	
+		//removendo elementos 
+		System.out.println("\n\nRemovendo elementos \n");
+		vet_animal =  ClasseTeste.remove(vet_animal, "thor");
+		vet_animal =  ClasseTeste.remove(vet_animal, "Carpeado2");
+		vet_animal =  ClasseTeste.remove(vet_animal, "Carpeado3");
+		vet_animal =  ClasseTeste.remove(vet_animal, "Preguica2");
+		vet_animal =  ClasseTeste.remove(vet_animal, "Preguica3");
 		
+		//vet_animal.forEach( animal -> System.out.println(animal) );
+		System.out.println(vet_animal);
+				
 		//para fins explicativos 
 //		vet_animal.get(0).setQntAnimal();
 //		vet_animal.get(0).setQntAnimal();
