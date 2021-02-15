00d1e530c7e4897b64424f113188c398099e8d54
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-02-15 13:03:05 -0300

    Create Pizzaria.DDL.sql

---
+++ src/semana6/dia1/Atividade 2/Pizzaria.DDL.sql
@@ -0,0 +1,20 @@
+-- CREATE DATABASE db_pizzaria_legal;
+USE db_pizzaria_legal;
+
+CREATE TABLE tb_categoria(
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL, 
+    categoria VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE tb_produto(
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nome_produto VARCHAR(50) NOT NULL, 
+    preco FLOAT NOT NULL, 
+    descricao VARCHAR(500) NOT NULL,
+    categoria_id INT(6) UNSIGNED NOT NULL,
+    CONSTRAINT `fk_categoria_id`
+     FOREIGN KEY (categoria_id) REFERENCES tb_categoria (id)
+     ON DELETE CASCADE
+     ON UPDATE RESTRICT
+);
